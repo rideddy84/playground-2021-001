276d54c1b8e8d37f4c6d10507eb5b9235fec28b6
Jimin Kim <rideddy84@gmail.com>
2021-04-02 16:57:33 +0900

    specify possible coupon types

--- server/src/coupons/coupon.entity.ts
+++ server/src/coupons/coupon.entity.ts
@@ -1,5 +1,9 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+export enum CouponType {
+  AMOUNT = "amount",
+  PERCENT = "percent"
+}
 @Entity()
 export class Coupon {
   @PrimaryGeneratedColumn()
@@ -8,6 +12,10 @@ export class Coupon {
   @Column()
   name: string;
 
-  @Column()
-  type: string;
+  @Column({
+    type: "enum",
+    enum: CouponType,
+    default: CouponType.AMOUNT
+  })
+  type: CouponType;
 }
